7f806b08552f7f98012746de735312461febda5d
Maxwell Little <maxwelllittle@maxwells-mbp.fios-router.home>
2018-04-10 17:46:03 -0400

    Added Some support

--- SAP/Support.swift
+++ SAP/Support.swift
@@ -20,3 +20,24 @@ func uniToChar(_ n: Int)-> Character {
 enum Command: Int{
     case halt, clrr, clrx, clmr, clrb, movir, movrr, movrm, movmr, movxr, movar, movb, addir, addrr, addmr, addxr, subir, subrr, submr, subxr, mulir, mulrr, mulmr, mulxr, divir, divrr, divmr, divxr, jmp, sojz, sojnz, aojz, aojnz, cmpir, cmprr, cmpmr, jmpn, jmpz, jmpp, jsr, ret, push, pop, stackc, outci, outcr, outcx, outcb, readi, printi, readc, readln, brk, movrx, movxx, outs, nop, jmpne
 }
+
+func readTextFile(_ path: String) -> (message: String?, fileText: String?) {
+    let text: String
+    
+    do {
+        text = try String(contentsOfFile: path, encoding: String.Encoding.utf8)
+    }
+        
+    catch {
+        return ("\(error)", nil)
+    }
+    return (nil, text)
+}
+
+func splitStringIntoParts(expression: String) -> [String] {
+    return expression.characters.split{$0 == " "}.map{ String($0) }
+}
+
+func splitStringIntoLines(expression: String) -> [String] {
+    return expression.characters.split{$0 == "\n"}.map{ String($0) }
+}
