c5127a8d139e0fca6be667b978eae07531769f0a
Mago Sheehy <magosheehy@Magos-MacBook-Pro.local>
2018-04-10 19:00:20 -0400

    az

--- SAP/main.swift
+++ SAP/main.swift
@@ -9,3 +9,21 @@
 import Foundation
 
 var SAP = Driver(Program: readTextFile("/Users/magosheehy/Documents/SAAAAP/SAP/Numbers.txt").1!)
+
+
+if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+    
+    let fileURL = dir.appendingPathComponent(file)
+    
+    //writing
+    do {
+        try text.write(to: fileURL, atomically: false, encoding: .utf8)
+    }
+    catch {/* error handling here */}
+    
+    //reading
+    do {
+        let text2 = try String(contentsOf: fileURL, encoding: .utf8)
+    }
+    catch {/* error handling here */}
+}
