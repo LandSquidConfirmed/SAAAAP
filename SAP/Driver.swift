c3cb29afd6684e12be8849e85a52308ae9d3fdb2
Mago Sheehy <magosheehy@Magos-MacBook-Pro.local>
2018-04-10 18:17:31 -0400

    asda

--- SAP/Driver.swift
+++ SAP/Driver.swift
@@ -27,7 +27,23 @@ struct Driver {
         
         
         
-        
+        func movmr(Int: label, Int: r){
+            registers[r] = mem[label]
+        }
+        func outs(Int: label){
+            for i in 1...mem[label]{
+                print(mem[label + i])
+            }
+        }
+        func outcr(Int: r){
+            print(registers[r])
+        }
+        func movrr(Int: r1, Int: r2){
+            registers[r2] = registers[r1]
+        }
+        func addrr(Int: r1, Int r2){
+            registers[r2] += registers[r1]
+        }
     }
     
 }
