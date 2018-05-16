545c9ee346a94aaf1526fc661ec8ead66d4f9d9a
Mago Sheehy <magosheehy@magos-mbp.newton.edu>
2018-05-16 15:08:45 -0400

    dada

--- SAP/main.swift
+++ SAP/main.swift
@@ -8,9 +8,8 @@
 
 import Foundation
 
-var SAP = Driver(Program: readTextFile("/Users/maxwelllittle/Library/Autosave Information/SAP/SAP/Numbers.txt").1!)
-var assem = Assembler()
 var SAP = Driver(Program: readTextFile("/Users/magosheehy/Desktop/Folder/Turing.bin").1!)
+/*var assem = Assembler()
 
 var file = readTextFile("/Users/maxwelllittle/Library/Autosave Information/SAP/SAP/Turing.txt")
 for e in tokenizeilator(splitFile(file: file.fileText!)) {
@@ -20,6 +19,6 @@ assem.passOne(tokenizeilator(splitFile(file: file.fileText!)))
 
 print(assem.bin)
 print(assem.symbolTable)
-
-//SAP.Driver()
+*/
+SAP.Driver()
 
